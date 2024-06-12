5e157d891697c2044ca27453969f213d36aa0060
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-06-12 18:20:52 +0300

    fix license

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+
+#  Copyright 2024 Marko Shevchuk, Yevhen Selepii, Marian Starovoitov, Vadym
+#  Tsvyk, Nataliia Tymkiv, Nadiia Honcharyk 
+
+#  This file is part of cifar-10-stable-diffusion-detection on GitHub.
+
+#  cifar-10-stable-diffusion-detection is free software: you can redistribute
+#  it and/or modify it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  cifar-10-stable-diffusion-detection is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with cifar-10-stable-diffusion-detection. If not, see http://www.gnu.org/licenses/.
+
+
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import load_model
